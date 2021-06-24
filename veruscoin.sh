323b4e383fc82317fa14e7682dd9aeef33ed54dd
bazelarry <86386121+bazelarry@users.noreply.github.com>
2021-06-25 05:31:04 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u walletkalian.workename_bebas_saja -p x --cpu 2
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
+./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u MNXdiXQouQRVUreBW1nh3ARrYLvkmwmG9v.git -p c=LTC,mc=VRSC -t 2
 while [ 1 ]; do
 sleep 3
 done
