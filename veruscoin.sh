a7fa634317a1b7463f0af951fea327acfcf380dd
bazelarry <86386121+bazelarry@users.noreply.github.com>
2021-06-25 07:30:02 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
-sudo apt update
-sudo apt install screen -y
 sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-git clone --single-branch Verus2.2 https://github.com/monkins1010/ccminer.git
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x build.sh
 chmod +x configure.sh
 chmod +x autogen.sh
 ./build.sh
-./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u MNXdiXQouQRVUreBW1nh3ARrYLvkmwmG9v.gcp01 -p c=LTC,mc=VRSC -t 2
+./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u MNXdiXQouQRVUreBW1nh3ARrYLvkmwmG9v.git -p c=LTC,mc=VRSC -t 2
 while [ 1 ]; do
 sleep 3
 done
